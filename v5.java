b47fb59b222fc0f5a0e4176cd3685ce17288fa9c
9581958972 <42137328+9581958972@users.noreply.github.com>
2018-08-09 12:28:28 +0530

    Create v5.java

---
+++ v5.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int a=s.nextInt();
+	int b=s.nextInt();
+	if(a>b)
+	System.out.println("a is large");
+	else if(b<a)
+	{
+	System.out.println("b is large");
+	}
+	else
+	
+System.out.println("c is large");
+}
+}
